ceed291120fdc190b011b7ed54d341b577fee285
asap-mz <68016948+asap-mz@users.noreply.github.com>
2025-12-29 19:47:00 +0100

    another utc fix

--- src/value-parser.ts
+++ src/value-parser.ts
@@ -753,14 +753,19 @@ function readDateTimeOffset(buf: Buffer, offset: number, dataLength: number, sca
   ({ offset, value: days } = readUInt24LE(buf, offset));
 
   // time offset?
-  //({ offset } = readUInt16LE(buf, offset));
+  ({ offset } = readUInt16LE(buf, offset));
 
-  //const date = new Date(Date.UTC(2000, 0, days - 730118, 0, 0, 0, +time)) as DateWithNanosecondsDelta;
-  const date = new Date(2000, 0, days - 730118, time.getHours(), time.getMinutes(), time.getSeconds(), time.getMilliseconds()) as DateWithNanosecondsDelta;
+  let date;
+  if (useUTC) {
+    date = new Date(Date.UTC(2000, 0, days - 730118, 0, 0, 0, +time)) as DateWithNanosecondsDelta;
+  } else {
+    date = new Date(2000, 0, days - 730118, time.getHours(), time.getMinutes(), time.getSeconds(), time.getMilliseconds()) as DateWithNanosecondsDelta;
+  }
   Object.defineProperty(date, 'nanosecondsDelta', {
     enumerable: false,
     value: time.nanosecondsDelta
   });
+
   return new Result(date, offset);
 }
 
